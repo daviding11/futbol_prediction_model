1a47272200775a17c3f61a73dd7c00d6dc9b7fdd
daviding11 <ing.david111@gmail.com>
2022-09-15 22:08:46 -0700

    added defense and possesion stats

--- scripts/web_scraper.py
+++ scripts/web_scraper.py
@@ -54,7 +54,6 @@ class match_scraper:
                 defense  = pd.read_html(data.text, match="Defensive Actions")[0]
                 defense.columns = defense.columns.map('_'.join)
                 defense.rename(columns={defense.columns[0]: 'Date'}, inplace=True)
-                #generic fields
                 try:
                     team_data = team_data.merge(
                         defense[["Date", "Pressures_Press", "Pressures_Succ", "Pressures_Def 3rd", "Pressures_Mid 3rd",
@@ -63,16 +62,36 @@ class match_scraper:
                 except ValueError:
                     continue
                 time.sleep(randint(1, 4))
+                #Possesion Stats
+                links = [l.get("href") for l in soup.find_all('a')]
+                links = [l for l in links if l and 'all_comps/possession/' in l]
+                data = requests.get(f"https://fbref.com{links[0]}")
+                possession = pd.read_html(data.text, match="Possession")[0]
+                possession.columns = possession.columns.map('_'.join)
+                possession.rename(columns={possession.columns[0]: 'Date'}, inplace=True)
+                try:
+                    team_data = team_data.merge(
+                        possession[["Date", "Touches_Touches", "Touches_Def Pen", "Touches_Mid 3rd",
+                                    "Touches_Att 3rd", "Touches_Att Pen", "Dribbles_Succ",
+                                    "Dribbles_Att",
+                                    "Dribbles_#Pl", "Carries_Carries", "Carries_TotDist",
+                                    "Carries_PrgDist",
+                                    "Carries_Prog", "Carries_1/3", "Carries_CPA", "Receiving_Targ",
+                                    "Receiving_Rec", "Receiving_Prog"]], on="Date")
+                except ValueError:
+                    continue
+                # generic fields
                 team_data["Season"] = year
                 team_data["Team"] = team_name
                 all_matches.append(team_data)
                 print(team_name + " success " + f"{year}")
-                time.sleep(randint(5, 10))
+                time.sleep(randint(1, 4))
                 #break
 
         match_df = pd.concat(all_matches)
         match_df.columns = [c.lower() for c in match_df.columns]
         match_df.to_csv(f"matches\{self.league}_matches.csv")
+        print(self.league + " done!")
 
 year1,year2 = 2022,2021
 prem_matches = match_scraper(year1,year2,'premier',"https://fbref.com/en/comps/9/Premier-League-Stats")
@@ -82,7 +101,7 @@ laliga_matches = match_scraper(year1,year2,'laliga',"https://fbref.com/en/comps/
 bundesliga_matches = match_scraper(year1,year2,'bundesliga',"https://fbref.com/en/comps/20/Bundesliga-Stats")
 
 prem_matches.web_scraper()
-ligue_1_matches.web_scraper()
-serie_a_matches.web_scraper()
-laliga_matches.web_scraper()
-bundesliga_matches.web_scraper()
+#ligue_1_matches.web_scraper()
+#serie_a_matches.web_scraper()
+#laliga_matches.web_scraper()
+#bundesliga_matches.web_scraper()
